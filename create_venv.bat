22166b6d7246c244a8876a30cd0a75981841c832
Jeison <jeison.arayamena@ucr.ac.cr>
2021-02-06 18:09:21 -0600

    Run as administrator bug solved

--- create_venv.bat
+++ create_venv.bat
@@ -10,18 +10,24 @@ echo =====================================
 echo   Taller introductorio Python 2021.  
 echo =====================================
 echo.
+:: Set Directory.
+set my_path=%~dp0
+set my_path=%my_path:~0, -1%
 :: Check python version.
-call python3 --version > temp.txt
-set /p python_version=<temp.txt
-del temp.txt
+set "temp_path=%my_path%\temp.txt"
+call python --version > %temp_path%
+set /p python_version=< %temp_path%
+del %temp_path%
 echo Version de python: %python_version%
 echo.
 :: Create python venv
 echo Creando entorno virtual...
-call python3 -m venv venv
+set "venv_path=%my_path%\venv"
+call python -m venv %venv_path%
 :: Check venv folder
-if exist venv (
-    echo Entorno virtual creado correctamente!
+if exist %venv_path% (
+    echo Entorno virtual creado correctamente
+    echo Directorio del entorno virtual: %venv_path%
     echo.
 ) else (
     echo Error al crear el entorno virtual.
@@ -30,20 +36,23 @@ if exist venv (
 :: Activate venv
 echo Iniciando entorno virtual...
 echo.
-call .\venv\Scripts\activate.bat
+call %venv_path%\Scripts\activate.bat
 :: Update pip
-echo Actualizando gestor de paquete de python (pip)...
+echo Actualizando gestor de paquetes de python (pip)...
 call pip install --upgrade pip
 color 02 
 echo.
 :: Check requirements file
-echo Comprobando modulos requeridos...
+echo =====================================
+echo   Comprobando modulos requeridos... 
+echo =====================================
 echo.
-if exist requirements.txt (
+set "requirements_path=%my_path%\requirements.txt"
+if exist %requirements_path% (
     :: Install modules required
     echo Instalando modulos adicionales...
     echo.
-    call pip install -r requirements.txt
+    call pip install -r %requirements_path%
 ) else (
     echo No se necesitan modulos adicionales.
     echo.
