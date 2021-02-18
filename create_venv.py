0ca4a095dafbb84af7d222dcac8f44f1f3467c9b
Jeison Araya <jeison.arayamena@ucr.ac.cr>
2021-02-18 05:21:31 -0600

    cleanning code

--- create_venv.py
+++ create_venv.py
@@ -6,19 +6,18 @@
 import os
 import sys
 import pip
-import stat
 import venv
 import platform
 
 from os import path
 from shutil import rmtree
 
-# Config.
+# Configuration.
 
-venv_name = 'venv'
-requirements_name = 'requirements.txt'
-box_messages_border = ''
+venv_dir_name = 'venv'
 scripts_dir_name = ''
+box_messages_border = ''
+requirements_file_name = 'requirements.txt'
 
 for x in range(50):
     box_messages_border += '='
@@ -37,22 +36,32 @@ def exec_command(command, get_output=False):
     os.system(command)
 
 def validate_python_version():
-    if not sys.version_info.major == 3 and sys.version_info.minor >= 6:
-        print('Python 3.6 o superior es necesario.')
-        print('Usted está utilizando Python {}.{}.'.format(sys.version_info.major, sys.version_info.minor))
+    if sys.version_info.major != 3 and sys.version_info.minor <= 7:
+        print('Python 3.7 o superior es necesario.')
+        print('Usted está utilizando Python {}.{}.'.format(sys.version_info.major, 
+                                                           sys.version_info.minor))
         sys.exit(1)
 
-def venv_exist(venv_path):
-    if path.exists(venv_path):
-        if path.isdir(venv_path):
-            return True
-    return False
+def validate_os():
+    global scripts_dir_name
+    operative_system = platform.system() 
+
+    if operative_system == 'Windows':
+        # Windows.
+        scripts_dir_name = 'Scripts'
+    elif operative_system == 'Linux' or operative_system == 'Darwin':
+        # Linux and Mac.
+        scripts_dir_name = 'bin'
+    else: 
+        # Other os.
+        input('Error: Sistema operativo no soportado.')
+        sys.exit(1)
+        
+def dir_exist(dir_path):
+    return path.exists(dir_path) and path.isdir(dir_path)
 
 def file_exists(file_path):
-    if path.exists(file_path):
-        if path.isfile(file_path):
-            return True
-    return False
+    return path.exists(file_path) and path.isfile(file_path)
 
 def create_venv(venv_path):
     venv.create(venv_path, clear=True, with_pip=True)
@@ -63,27 +72,41 @@ def update_venv(venv_path):
 
 def install_modules(venv_path, requirements_path):
     global scripts_dir_name
-    exec_command(path.join(venv_path, scripts_dir_name, 'pip install -r {}'.format(requirements_path)))    
+    # Verify if file exists.
+    if file_exists(requirements_path):
+        print('Archivo de dependencias encontrado.')
+        print('Instalando dependencias...')
+        exec_command(path.join(venv_path, scripts_dir_name, 
+                               'pip install -r {}'.format(requirements_path)))    
+    else:
+        print('No se requiere instalar ninguna dependencia.')
 
+def validate_modules_required(venv_path, requirements_path):
+    global scripts_dir_name
+    if file_exists(requirements_path):
+        # Get modules required
+        file = open(requirements_path, "r")
+        modules_required = file.read()
+        # Get modules installed
+        modules_installed = exec_command(path.join(venv_path, scripts_dir_name, 'pip freeze'), 
+                                                   get_output=True)    
+        return modules_installed == modules_required
+    else:
+        # Dependencies are not required.
+        return True
+    
 # Check python version.
+
 validate_python_version()
 
 # Check operative system.
 
-operative_system = platform.system() 
-
-if operative_system == 'Windows':
-    scripts_dir_name = 'Scripts'
-elif operative_system == 'Linux' or operative_system == 'Darwin':
-    scripts_dir_name = 'bin'
-else: 
-    input('Error: Sistema operativo no soportado.')
-    sys.exit(1)
+validate_os()
 
 # Paths 
 
 dir_path = path.dirname(path.realpath(__file__))
-venv_path = path.join(dir_path, venv_name)
+venv_path = path.join(dir_path, venv_dir_name)
 
 # Init message.
 
@@ -92,7 +115,7 @@ print('Iniciando programa para crear y configurar el entorno virtual de programa
 
 # Check if there is an old venv.
 
-if venv_exist(venv_path):
+if dir_exist(venv_path):
     print('Se encontró un entorno virtual.')
     print('Eliminando configuración existente...')
 
@@ -102,7 +125,7 @@ show_message('Creando entorno virtual')
 print('Por favor, espere unos segundos...')
 create_venv(venv_path)
 
-if venv_exist(venv_path):
+if dir_exist(venv_path):
     print('Entorno virtual creado correctamente.')
 else:
     input('Error: No se ha creado el entorno virtual.')
@@ -116,17 +139,16 @@ print('Se ha actualizado pip correctamente.')
 
 # Install modules required.
 
-requirements_path = path.join(dir_path, requirements_name)
+requirements_path = path.join(dir_path, requirements_file_name)
 
 show_message('Instalando dependencias.')
-
-if file_exists(requirements_path):
-    print('Archivo de dependencias encontrado.')
-    print('Instalando dependencias...')
-    install_modules(venv_path, requirements_path)
-    print('Dependencias instaladas correctamente.')
+install_modules(venv_path, requirements_path)
+if validate_modules_required(venv_path, requirements_path):
+    print('Las dependencias necesarias han sido instaladas.')
 else:
-    print('No se requiere instalar ninguna dependencia.')
+    print('No se han logrado instalar todas las dependencias.')
+
+# End message.
 
 show_message('Entorno virtual configurado y listo para ser utilizado.')
 
